d598d072f4287d2ab0b76e7ea230db90a6605681
moniratna <moniratna.munshi123@gmail.com>
2025-03-13 13:39:38 +0530

    added new api end point to get balance sheet data

--- server/config.py
+++ server/config.py
@@ -8,5 +8,7 @@ class Config:
     @property
     def data_file_path(self) -> str:
         return os.getenv("DATA_FILE_PATH", "../assets/profit-and-loss.json")  # Default value if not set
-
+    @property
+    def data_balance_sheet(self) -> str:
+        return os.getenv("DATA_BALANCE_SHEET", "../assets/balance-sheet.json")  # Default value if not set
 config = Config()
--- server/main.py
+++ server/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from service import read_profit_and_loss_data
+from service import read_balance_sheet_data
 from config import config
 from typing import List
 import uvicorn
@@ -7,11 +9,30 @@ import json
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Change this to your frontend URL in production
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all HTTP methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allow all headers
+)
+
 @app.get("/quickbooks/profit-and-loss")
 async def read_profit_and_loss():
     file_path = config.data_file_path
     try:
         result = read_profit_and_loss_data(file_path)
+        return result
+    except FileNotFoundError:
+        raise HTTPException(status_code=404, detail="File not found")
+    except json.JSONDecodeError:
+        raise HTTPException(status_code=500, detail="Error reading JSON data")
+
+@app.get("/quickbooks/balance-sheet")
+async def read_profit_and_loss():
+    file_path = config.data_balance_sheet
+    try:
+        result = read_balance_sheet_data(file_path)
         return result
     except FileNotFoundError:
         raise HTTPException(status_code=404, detail="File not found")
